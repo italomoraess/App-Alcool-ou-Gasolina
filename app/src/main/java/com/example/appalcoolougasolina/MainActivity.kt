5dd7bc47d90c26b0e0a2ac7be75ce7e559cbac89
Italo Moraes <italomoraes.sth@gmail.com>
2023-09-26 22:40:51 -0300

    Starting Project

---
+++ app/src/main/java/com/example/appalcoolougasolina/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.appalcoolougasolina
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
